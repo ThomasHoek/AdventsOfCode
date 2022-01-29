1c518f6aab1fb455bf4172393e3f9ee1fd8c2468
ThomasHoek <hoek.thomas@gmail.com>
2022-01-29 16:25:51 +0100

    github reformatting

---
+++ 2015/day 15/day15.py
@@ -0,0 +1,6 @@
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+puzzle_input = open(f"{dir_path}/input.txt", "r").readlines()
+
+puzzle_input = [x.rstrip() for x in puzzle_input]
